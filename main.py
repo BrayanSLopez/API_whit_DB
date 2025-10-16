f09f711616b9732a13ff938e97406dc1c789c6cf
Brayan <146513370+BrayanSLopez@users.noreply.github.com>
2025-10-16 03:38:14 +0000

    Modificacion de main agregando la configuracion de JWT y demas

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from flask import Flask
-from controllers.product_controllers import product_bp 
-from controllers.user_controllers import user_bp
+from config.jwt import *
+from config.database import engine
+from models.product_model import Base  # Asegúrate que Base esté correctamente importado
+from controllers.product_controllers import product_bp
+from controllers.user_controllers import user_bp, register_jwt_error_handlers
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
-# Registrar el blueprint de productos
+# Configuración de JWT
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
+app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME
+app.config['JWT_HEADER_TYPE'] = JWT_HEADER_TYPE
+
+jwt = JWTManager(app)
+
+# Registrar blueprints
 app.register_blueprint(product_bp)
 app.register_blueprint(user_bp)
 
+# Registrar manejadores personalizados de error JWT
+register_jwt_error_handlers(app)
+
 if __name__ == "__main__":
+    # Crear tablas automáticamente si no existen
+    print("Verificando y creando tablas de base de datos si es necesario...")
+    Base.metadata.create_all(engine)
+    print("Tablas listas.")
     app.run(debug=True)
